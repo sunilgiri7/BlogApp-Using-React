3c1eaa571f846d2df900aa69caa36c59bea6890e
sunilgiri7 <seungiri841@gmail.com>
2024-03-09 03:13:57 -0800

    'crud'

--- api/index.js
+++ api/index.js
@@ -3,6 +3,11 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path"); // Import the path module
 
 dotenv.config();
 app.use(express.json());
@@ -16,7 +21,24 @@ mongoose
     console.error("Error connecting to MongoDB:", err);
   });
 
-  app.use("/api/auth", authRoute);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images"); // Assuming 'images' folder exists in the project directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name); // Use a unique filename based on the request body
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running");
